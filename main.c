4f45a7806fada2868948f05c4b6bfefb56f3b407
Felipe Muraro Silva Vilela <felipe.vilela@discente.uenp.edu.br>
2025-01-10 13:44:04 -0300

    menu

--- main.c
+++ main.c
@@ -7,8 +7,12 @@ int main(){
     int opcao;
 
     do{
-        printf("\n ~~~ ");
-    }
+        printf("1 - Cadastrar Insumo\n");
+        printf("2 - Alterar Cadastro de Insumo\n");
+        printf("3 - Excluir Cadastro de Insumo\n");
+        printf("4 - Listar Insumos\n");
+        printf("0 - Sair\n");
+    }while(opcao != 0);
 
 
     return 0;
