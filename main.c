0303b2e171b8759f9f704e7f7ceb67a0ee2fc9a7
JJUUNN0 <sophiater7@gmail.com>
2025-01-10 14:34:26 -0300

    funcao cadastrar Insumo

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
 #include<stdio.h>
 
-void cadastrarInsumo();
+typedef struct{
+	char nome[50];
+	char categoria [30];
+	char unidadeDeMedida [20];
+	float quantidade;
+}Insumo;
+
+void cadastrarInsumo (){
+	FILE *arquivo = fopen("insumos.txt", "a");
+    if(arquivo == NULL){
+        printf("ERRO ao abrir o arquivo\n");
+        return;
+    }
+
+    Insumo insumo;
+    printf("Nome do Produto: ");
+    scanf(" %[^\n]", insumo.nome);
+    printf("Categoria: ");
+    scanf(" %[^\n]", insumo.categoria);
+    printf("Unidade de Medida (kg, litros, unidades): ");
+    scanf(" %[^\n]", insumo.unidadeDeMedida);
+    printf("Quantidade: ");
+    scanf("%f", &insumo.quantidade);
+
+    fprintf(arquivo, "%s;%s;%s;%.2f\n", insumo.nome, insumo.categoria, insumo.unidadeDeMedida, insumo.quantidade);
+    fclose(arquivo);
+
+    printf("Cadastrado do insumo realizado com sucesso!\n");
+}
 
 
 int main(){
